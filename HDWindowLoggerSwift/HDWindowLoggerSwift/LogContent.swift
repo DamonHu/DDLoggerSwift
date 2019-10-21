aaf2fbc3124e485142c969db514c8bffa5dbdbbc
Damon <dong765@qq.com>
2019-10-21 09:40:54 +0800

    增加URL解析
    增加URL解析

--- HDWindowLoggerSwift/HDWindowLoggerSwift/LogContent.swift
+++ HDWindowLoggerSwift/HDWindowLoggerSwift/LogContent.swift
@@ -8,15 +8,17 @@
 
 import Foundation
 
+//输出内容需要遵循的协议
 public protocol LogContent {
     var logStringValue: String { get }
 }
 
+///默认的几个输出类型
 extension Dictionary: LogContent {
     public var logStringValue: String {
         let data = try? JSONSerialization.data(withJSONObject: self, options:JSONSerialization.WritingOptions.prettyPrinted)
         let defaultData = Data()
-        return String(data: data ?? defaultData, encoding: String.Encoding.utf8) ?? ""
+        return String(data: data ?? defaultData, encoding: String.Encoding.utf8) ?? "不支持json解析的Dictionary"
     }
 }
 
@@ -24,12 +26,18 @@ extension Array: LogContent {
     public var logStringValue: String {
         let data = try? JSONSerialization.data(withJSONObject: self, options:JSONSerialization.WritingOptions.prettyPrinted)
         let defaultData = Data()
-        return String(data: data ?? defaultData, encoding: String.Encoding.utf8) ?? ""
+        return String(data: data ?? defaultData, encoding: String.Encoding.utf8) ?? "不支持json解析的array类型"
     }
 }
 
 extension String: LogContent {
     public var logStringValue: String {
         return self
+    }
+}
+
+extension URL: LogContent {
+    public var logStringValue: String {
+        return self.absoluteString
     }
 }
