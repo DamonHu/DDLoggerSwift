42fa734b5ed52ae4930b0f734ccabc533c00c122
Damon <dong765@qq.com>
2021-03-13 23:12:13 +0800

    Update HDLoggerWindow.swift

--- HDWindowLoggerSwift/HDWindowLoggerSwift/HDLoggerWindow.swift
+++ HDWindowLoggerSwift/HDWindowLoggerSwift/HDLoggerWindow.swift
@@ -16,7 +16,6 @@ class HDLoggerWindow: UIWindow {
     private var mCurrentSearchIndex = 0             //当前搜索到的索引
     private var mFileDateNameList = [String]()      //可以分享的文件列表
     private var mShareFileName = ""                 //选中去分享的文件名
-    var dataSource: Any?
     
     private let logQueue = DispatchQueue(label: "HDWindowLogger")
     var mFloatButton: UIButton?
@@ -35,13 +34,9 @@ class HDLoggerWindow: UIWindow {
                     self.mLogDataArray.append(loggerItem)
                 }
                 self.p_reloadFilter()
-                if self.mAutoScrollSwitch.isOn {
-                    guard self.mLogDataArray.count > 1 else { return }
-                    self.mTableView.scrollToRow(at: IndexPath(row: self.mLogDataArray.count - 1, section: 0), at: .bottom, animated: true)
-                }
             } else {
                 self.isHidden = true
-                self.mFloatWindow.isHidden = true
+                self.mFloatWindow.isHidden = false
             }
         }
     }   //显示窗口
@@ -65,48 +60,13 @@ class HDLoggerWindow: UIWindow {
     }
 
     func insert(model: HDWindowLoggerItem) {
-        if #available(iOS 13.0, *) {
-            self.logQueue.sync {
-                if HDWindowLoggerSwift.mMaxShowCount != 0 && self.mLogDataArray.count > HDWindowLoggerSwift.mMaxShowCount {
-                    self.mLogDataArray.removeFirst()
-                }
-                self.mLogDataArray.append(model)
-                if self.isShow {
-                    guard let dataSource = dataSource as? UITableViewDiffableDataSource<Section, HDWindowLoggerItem> else {
-                        self.dataSource = self.getDataSource()
-                        return
-                    }
-                    dataSource.defaultRowAnimation = .fade
-                    var snapshot = dataSource.snapshot()
-                    if snapshot.numberOfSections == 0 {
-                        snapshot.appendSections([.main])
-                    }
-                    snapshot.appendItems(mLogDataArray)
-                    dataSource.apply(snapshot, animatingDifferences: false)
-                    if self.mAutoScrollSwitch.isOn {
-                        guard self.mLogDataArray.count > 1 else { return }
-                        DispatchQueue.main.async {
-                            self.mTableView.scrollToRow(at: IndexPath(row: self.mLogDataArray.count - 1, section: 0), at: .bottom, animated: true)
-                        }
-                    }
-                }
+        self.logQueue.sync {
+            if HDWindowLoggerSwift.mMaxShowCount != 0 && self.mLogDataArray.count > HDWindowLoggerSwift.mMaxShowCount {
+                self.mLogDataArray.removeFirst()
             }
-            
-        } else {
-            self.logQueue.sync {
-                if HDWindowLoggerSwift.mMaxShowCount != 0 && self.mLogDataArray.count > HDWindowLoggerSwift.mMaxShowCount {
-                    self.mLogDataArray.removeFirst()
-                }
-                self.mLogDataArray.append(model)
-                if self.isShow {
-                    self.p_reloadFilter()
-                    if self.mAutoScrollSwitch.isOn {
-                        guard self.mLogDataArray.count > 1 else { return }
-                        DispatchQueue.main.async {
-                            self.mTableView.scrollToRow(at: IndexPath(row: self.mLogDataArray.count - 1, section: 0), at: .bottom, animated: true)
-                        }
-                    }
-                }
+            self.mLogDataArray.append(model)
+            if self.isShow {
+                self.p_reloadFilter(model: model)
             }
         }
     }
@@ -120,12 +80,13 @@ class HDLoggerWindow: UIWindow {
 
     //只隐藏log的输出窗口，保留悬浮图标
     @objc func hideLogWindow() {
-        self.isHidden = true
-        self.mFloatWindow.isHidden = false
+        self.isShow = false
     }
 
     @objc func hide() {
         self.isShow = false
+        self.isHidden = true
+        self.mFloatWindow.isHidden = true
     }
     
     //MARK: UI布局
@@ -373,7 +334,7 @@ private extension HDLoggerWindow {
     }
 
     //过滤刷新
-    private func p_reloadFilter() {
+    private func p_reloadFilter(model: HDWindowLoggerItem? = nil) {
         self.mFilterIndexArray.removeAll()
         self.mPreviousButton.isEnabled = false
         self.mNextButton.isEnabled = false
@@ -394,6 +355,12 @@ private extension HDLoggerWindow {
             }
         }
         self.mTableView.reloadData()
+        if self.mAutoScrollSwitch.isOn {
+            guard self.mLogDataArray.count > 1 else { return }
+            DispatchQueue.main.async {
+                self.mTableView.scrollToRow(at: IndexPath(row: self.mLogDataArray.count - 1, section: 0), at: .bottom, animated: true)
+            }
+        }
     }
 
     @objc private func p_previous() -> Void {
@@ -750,22 +717,5 @@ extension HDLoggerWindow: UIPickerViewDelegate, UIPickerViewDataSource {
             self.mShareFileName = self.mFileDateNameList[row]
         }
 
-    }
-}
-
-
-@available(iOS 13.0, *)
-extension HDLoggerWindow {
-    func getDataSource() -> UITableViewDiffableDataSource<Section, HDWindowLoggerItem>? {
-            return UITableViewDiffableDataSource(tableView: self.mTableView) { (tableView, indexPath, loggerItem) -> UITableViewCell? in
-                let loggerCell: HDLoggerSwiftTableViewCell = tableView.dequeueReusableCell(withIdentifier: "HDLoggerSwiftTableViewCell") as! HDLoggerSwiftTableViewCell
-                if indexPath.row%2 != 0 {
-                    loggerCell.backgroundColor = UIColor(red: 156.0/255.0, green: 44.0/255.0, blue: 44.0/255.0, alpha: 0.8)
-                } else {
-                    loggerCell.backgroundColor = UIColor.clear
-                }
-                loggerCell.updateWithLoggerItem(loggerItem: loggerItem, highlightText: self.mSearchBar.text ?? "")
-                return loggerCell
-            }
     }
 }
