44bc381f9b0e4c6a62180aed08ef498a59ae0658
Damon <dong765@qq.com>
2025-03-24 18:33:07 +0800

    Update DDLoggerSwiftSocketManager.swift

--- pod/socket/DDLoggerSwiftSocketManager.swift
+++ pod/socket/DDLoggerSwiftSocketManager.swift
@@ -9,6 +9,8 @@
 import Foundation
 import Network
 import UIKit
+import CommonCrypto
+import DDUtils
 
 
 @available(iOS 13.0, *)
@@ -16,9 +18,7 @@ class DDLoggerSwiftSocketManager: NSObject {
     public static let shared = DDLoggerSwiftSocketManager()
     private var netService: NetService? //发布bonjour服务
     var listener: NWListener?           //监听连接
-    //socket
-    var webSocketTaskList: [URLSessionWebSocketTask] = []
-    let session = URLSession(configuration: .default)
+    var connectList: [NWConnection] = []
 }
 
 @available(iOS 13.0, *)
@@ -32,94 +32,131 @@ extension DDLoggerSwiftSocketManager {
                                 port: DDLoggerSwift.socketPort)
         netService?.delegate = self
         netService?.publish()
+        self.httpListener()
     }
     
-    func startWebSocketServer() {
-        print("sssss", self.getLocalIPAddress())
-        let url = URL(string: "ws://\(getLocalIPAddress()):\(DDLoggerSwift.socketPort)")! // 监听所有 IP
-        let webSocketTask = session.webSocketTask(with: url)
-        self.webSocketTaskList.append(webSocketTask)
-        // 启动 WebSocket 任务
-        webSocketTask.resume()
-        // 接收消息
-        self.receiveMessage(webSocketTask)
-    }
-    
-    func getLocalIPAddress() -> String {
-        var address: String = "127.0.0.1"
-        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+    func httpListener() {
+        let parameters = NWParameters.tcp
+        parameters.includePeerToPeer = true
+        do {
+            listener = try NWListener(using: parameters, on: NWEndpoint.Port("\(DDLoggerSwift.socketPort)") ?? 0) // 任意端口
+            listener?.service = NWListener.Service (name: DDLoggerSwift.userID + "-" + UIDevice.current.name, type: "\(DDLoggerSwift.socketType)._tcp", domain: "local.")
+            listener?.stateUpdateHandler = { state in
+                switch state {
+                case .ready:
+                    print("Service is ready.")
+                case .failed(let error):
+                    print("Service failed with error: \(error)")
+                default:
+                    break
+                }
+            }
             
-            if getifaddrs(&ifaddr) == 0, let firstAddr = ifaddr {
-                var ptr = firstAddr
-                while ptr.pointee.ifa_next != nil {
-                    defer { ptr = ptr.pointee.ifa_next! }
-                    let interface = ptr.pointee
-                    let addrFamily = interface.ifa_addr.pointee.sa_family
-                    
-                    if addrFamily == AF_INET { // 仅获取 IPv4 地址
-                        let name = String(cString: interface.ifa_name)
-                        if name == "en0" { // en0 代表 Wi-Fi 适配器
-                            var addr = interface.ifa_addr.withMemoryRebound(to: sockaddr_in.self, capacity: 1) { $0.pointee }
-                            address = String(cString: inet_ntoa(addr.sin_addr))
-                            break
-                        }
-                    }
-                }
-                freeifaddrs(ifaddr)
+            listener?.newConnectionHandler = { [weak self] newConnection in
+                print("New connection from \(newConnection.endpoint)")
+                guard let self = self else { return }
+                self.handleNewConnection(newConnection)
             }
-            return address
+            listener?.start(queue: .global())
+        } catch {
+            print("DDLoggerSwift_accept error", error)
+        }
     }
     
-    private func receiveMessage(_ websocket: URLSessionWebSocketTask) {
-            websocket.receive { [weak self] result in
-                switch result {
-                case .success(let message):
-                    switch message {
-                    case .string(let text):
-                        print("Received message: \(text)")
-                    case .data(let data):
-                        if let logMessage = String(data: data, encoding: .utf8) {
-                            print("Received log message: \(logMessage)")
-                            if (logMessage == "DDLoggerClient_tcp_auth") {
-                                //发送历史消息
-                                print("sssss", DDLoggerSwift.getAllLog().count)
-                                for loggerItem in DDLoggerSwift.getAllLog() {
-                                    self?.send(loggerItem: loggerItem)
-                                }
+    // 处理新连接
+    func handleNewConnection(_ connection: NWConnection) {
+        connection.start(queue: .global())
+        self.connectList.append(connection)
+//        self.receiveLogMessages(from: connection)
+        performWebSocketHandshake(connection)
+    }
+    
+    private func performWebSocketHandshake(_ connection: NWConnection) {
+            connection.receive(minimumIncompleteLength: 1, maximumLength: 1024) { data, _, _, error in
+                if let data = data, let request = String(data: data, encoding: .utf8) {
+                    print("📩 收到握手请求:\n\(request)")
+
+                    if request.contains("Upgrade: websocket") {
+                        let response = """
+                        HTTP/1.1 101 Switching Protocols\r
+                        Upgrade: websocket\r
+                        Connection: Upgrade\r
+                        Sec-WebSocket-Accept: \(self.generateWebSocketAcceptKey(request))\r
+                        \r
+                        """
+                        connection.send(content: response.data(using: .utf8), completion: .contentProcessed({ error in
+                            if let error = error {
+                                print("❌ 握手失败: \(error)")
+                            } else {
+                                print("✅ WebSocket 握手成功!")
+                                self.send(loggerItem: DDLoggerSwiftItem())
+                                self.receiveLogMessages(from: connection)
                             }
-                        }
-                    @unknown default:
-                        break
-                    }
-                    // 继续接收下一条消息
-                    self?.receiveMessage(websocket)
-                case .failure(let error):
-                    print("Error receiving message: \(error)")
-                    if let index = self?.webSocketTaskList.firstIndex(of: websocket) {
-                        self?.webSocketTaskList.remove(at: index)
+                        }))
                     }
                 }
             }
+        }
+
+        private func generateWebSocketAcceptKey(_ request: String) -> String {
+            let keyPrefix = "Sec-WebSocket-Key: "
+            guard let keyRange = request.range(of: keyPrefix) else { return "" }
+            let keyLine = request[keyRange.upperBound...].split(separator: "\r").first ?? ""
+            let key = String(keyLine).trimmingCharacters(in: .whitespaces)
+            let magicString = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+            let hash = (key + magicString).dd.hashString(hashType: .sha1)?.data(using: .utf8)
+            return hash!.base64EncodedString()
         }
     
     func send(loggerItem: DDLoggerSwiftItem) {
         guard let data = "\(loggerItem.mLogItemType.rawValue)#\(loggerItem.mLogDebugContent)#\(loggerItem.mCreateDate.timeIntervalSince1970)#\(loggerItem.getLogContent())".data(using: .utf8) else { return
         }
-        
-        for task in self.webSocketTaskList {
-            task.send(URLSessionWebSocketTask.Message.data(data)) { error in
+        let frameHeader = Data([0x81])  // WebSocket 控制字节，用于文本帧
+                let messageLength = Data([UInt8(data.count)])
+
+                var dataToSend = frameHeader
+                dataToSend.append(messageLength)
+                dataToSend.append(data)
+        for connection in self.connectList {
+            connection.send(content: dataToSend, completion: .contentProcessed { error in
                 if let error = error {
-                    print("Failed to send data: \(error)")
+                    print("❌ 发送消息失败: \(error)")
                 } else {
-                    print("Message sent successfully.")
+                    print("📤 发送消息成功")
+                }
+            })
+        }
+    }
+    
+    
+    
+    // 接收日志
+    func receiveLogMessages(from connection: NWConnection) {
+        connection.receive(minimumIncompleteLength: 1, maximumLength: 1024) { [weak self] data, context, isComplete, error in
+            if let error = error {
+                print("Receive error: \(error)")
+                return
+            }
+            if let data = data, let logMessage = String(data: data, encoding: .utf8), logMessage == "DDLoggerClient_tcp_auth" {
+                print("Received log message: \(logMessage)")
+                //发送历史消息
+                print("sssss", DDLoggerSwift.getAllLog().count)
+                for loggerItem in DDLoggerSwift.getAllLog() {
+                    self?.send(loggerItem: loggerItem)
                 }
             }
+            // 继续接收数据
+            self?.receiveLogMessages(from: connection)
         }
     }
     
     func stop() {
+        listener?.cancel()  // 取消监听
+        listener = nil
         
     }
+    
+    
 }
 
 
@@ -130,8 +167,6 @@ extension DDLoggerSwiftSocketManager: NetServiceDelegate {
             print("Service name: \(sender.name)")
             print("Service type: \(sender.type)")
             print("Service port: \(sender.port)")
-        
-        self.startWebSocketServer()
         }
         
         func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
